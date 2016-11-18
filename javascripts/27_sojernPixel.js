4e3e8c2ec8579cd2180baf24b8491234722bbeb5
Insik Cho <insik.cho@alaskaair.com>
2016-11-18 14:51:54 -0800

    Revert "default config"
    This reverts commit 6b2cccc0d12c72074775b2c804bb8c2b86303b03.

--- javascripts/27_sojernPixel.js
+++
@@ -1,9 +0,0 @@
-function SojernPixel() {
-    this.insertSojernPixel = function () {
-        if ($('#sojernPixelUrl').length) {
-            $('body').append($("<img width='1' height='1' border='0' alt='sojern pixel url'></img>").attr({ src: $('#sojernPixelUrl').attr("href") }));
-        }
-    }
-}
-
-as.sojernPixel = new SojernPixel();
--- javascripts/43_PreventDoubleClicks.js
+++
@@ -1,7 +0,0 @@
-function do_nothing() { return false; } // prevent a second click for 10 seconds.
-$(document).delegate('.nodblclick', 'click', function (e) {
-    $(e.target).click(do_nothing);
-    setTimeout(function () {
-        $(e.target).unbind('click', do_nothing);
-    }, 10000);
-}); 
